4146f0dd0012d76d0e0b505a492258a5995cecaa
wthoutjc <juancamilorr2012@hotmail.com>
2022-06-12 19:14:11 -0500

    base url fixed

--- api/request.ts
+++ api/request.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://crud-db1-app.herokuapp.com/api' : 'http://localhost:3000/api'
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://virtus-training.vercel.app/' : 'http://localhost:3000/api'
 
 const request = axios.create({
   baseURL: BASE_URL,
